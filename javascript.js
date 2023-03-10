53d02ea29a6cc1944a24502758bfe98f0f8d56d6
nehldotjs <nelsonosuya11@gmail.com>
2023-03-10 18:30:00 +0100

    NEW COMMIT

--- javascript.js
+++ javascript.js
@@ -10,16 +10,13 @@ const gender =document.getElementById("genderInput")
 form.addEventListener("submit", async (e) => {
   // Prevent the default form submission behavior
   e.preventDefault();
-
   // Define a function that performs an action and returns a promise
   async function performAction() {
     // Perform the action here and wait for it to complete
     const result = await someAction(console.log("MSSG FROM CONSOLE"));
-
     // Return a resolved promise with the result
     return Promise.resolve("Action completed successfully!");
   }
-
   try {
     // Call the performAction function and wait for it to complete
     const result = await performAction();
